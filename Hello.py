98167b64c9f83a58cbe08c6f5aa2476833b38365
OniiSean <subombay@gmail.com>
2024-10-18 14:02:35 -0700

    I add fortnite battle pass

--- Hello.py
+++ Hello.py
@@ -4,4 +4,5 @@ number = 10 + x
 number = number*10
 print("Hi this is my number",number)
 print(number)
-
+print("Fortnite Battle Pass")
+print("This is Sean's Pull Request, only added two new print lines")
