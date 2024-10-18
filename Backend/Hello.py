c495719cc120552e2472f22d5efb844d7bc5a777
hyemdanu <hoedison2003@gmail.com>
2024-10-18 14:14:03 -0700

    hi im edison

--- Backend/Hello.py
+++ Backend/Hello.py
@@ -1,9 +1,1 @@
-print("Hello World")
-x = 2
-number = 10 + x
-number = number*10
-print("Hi this is my number",number)
-print(number)
-print("this is my number" , number)
-print("hi")
-print("Hi im edison")
+print("PULL REQUEST")
