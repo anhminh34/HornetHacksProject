d328d6f231e5b36e73a66fc1b0ea04b97c511f5e
Daniel89987 <mxsl003@icloud.com>
2024-10-18 18:23:58 -0700

    BRUHBRUHBRUH

--- Backend/Hello.py
+++ Backend/Hello.py
@@ -1,3 +1,10 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text2text-generation", model="google/flan-t5-large")
+
+
+
 print("PULL REQUEST")
 #hi pookie
 print("Sean's PULL REQUEST")
