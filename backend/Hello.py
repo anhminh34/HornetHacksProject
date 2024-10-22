dee285da3cd988ad3b1471e729ff82c2a1ba4cb7
anhminh34 <anhminh34@gmail.com>
2024-10-21 22:14:50 -0700

    renamed folders and implemented react project in ui folder

---
+++ backend/Hello.py
@@ -0,0 +1,14 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text2text-generation", model="google/flan-t5-large")
+
+
+
+print("PULL REQUEST")
+#hi pookie
+print("Sean's PULL REQUEST")
+#hi matty watty
+#Hi sean
+print(":)")
+
