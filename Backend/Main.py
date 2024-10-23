6c9ea1739ae828a17138e4b6f3fc70b2ac3e759b
anhminh34 <anhminh34@gmail.com>
2024-10-23 00:01:48 -0700

    added FB model

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,10 +1,8 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large")
+pipe = pipeline("text2text-generation", model="facebook/blenderbot-400M-distill")
 
-input_text = "translate English to German: How old are you?"
-input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-
-outputs = model.generate(input_ids)
-print(tokenizer.decode(outputs[0]))
+response = "You look very ugly, what is wrong with you?"
+result = pipe(response)
+print(result)
